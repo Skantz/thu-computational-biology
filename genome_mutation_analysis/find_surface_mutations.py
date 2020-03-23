f69fc65a36e3723e918e1325f927fdd3311d445e
Skantz <danska@kth.se>
2020-03-24 03:14:35 +0900

    merge files

--- genome_mutation_analysis/find_surface_mutations.py
+++ genome_mutation_analysis/find_surface_mutations.py
@@ -1,3 +1,14 @@
+import csv
+import json
+from ast import literal_eval
+
+from pymol import cmd
+
+
+
+
+###################
+
 '''
 http://pymolwiki.org/index.php/FindSurfaceResidues
 
@@ -11,10 +22,6 @@ Redistribution and use in source and binary forms, with or without modification,
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
-
-from __future__ import print_function
-from pymol import cmd
-
 
 def findSurfaceAtoms(selection="all", cutoff=2.5, quiet=1):
     """
@@ -102,3 +109,65 @@ RETURNS
 cmd.extend("findSurfaceAtoms", findSurfaceAtoms)
 cmd.extend("findSurfaceResidues", findSurfaceResidues)
 
+
+#######################
+
+
+
+HEADERS_PROT = ["S", "ORF3a", "ORF8"]
+HEADERS_MUT  = ["S Mutations", "orf3a Mutations", "orf8 Mutations"]
+
+cmd.load("QHD43416.pdb", "S")
+cmd.load("QHD43417.pdb", "ORF3a")
+cmd.load("QHD43422.pdb", "ORF8")
+
+content = {}
+
+mat = {n:[] for n in HEADERS_MUT + HEADERS_PROT}
+
+dr_content = []
+with open("DataTable.tsv", "r") as csvf:
+    df = csv.DictReader(csvf, delimiter='\t') #names=(HEADERS_PROT + HEADERS_MUT))
+    for i, line in enumerate(df):
+        op_line = line
+        for j, kv_tup in enumerate(line):
+            if kv_tup in HEADERS_MUT:
+                #print(kv_tup)
+                line[kv_tup] = literal_eval(line[kv_tup])
+        dr_content.append(op_line)
+
+selection_names = []
+mutations = []
+
+for i, line in enumerate(dr_content):
+    #print(line)
+    for j, key in enumerate(line):
+        #print(line[kv_pair])
+        if key not in (HEADERS_MUT):
+            continue
+        for k, mut in enumerate(line[key]):
+            name = key.split()[0] + "_" +  str(i)
+            cmd.select(name, "resi " + str(mut[1]) + " in " + key.split()[0])
+            mutations.append(mut)
+            selection_names.append(name)
+
+cmd.hide("all")
+cmd.show("surface")
+
+#for prot in HEADERS_PROT:
+#    cmd.alter(prot, "vdw=" + str(0.1))
+
+
+sr_selname = findSurfaceResidues("all", 2.5, 1)
+
+exposed_res = [tup[1] for tup in sr_selname]
+mutation_index = [trip[1] for trip in mutations]
+        
+exposed_mut = [n for n in mutation_index if n in exposed_res]
+
+print(len(exposed_mut)/len(mutations))
+
+print(mutations)
+for name in selection_names:
+    cmd.alter(name, "cdw=" + str(2))
+    cmd.color("red", name)
